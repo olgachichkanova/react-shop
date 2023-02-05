e47d063453e86995d0ee76c650304e54d9109087
Olga Andriychenko <olga@glasnostic.com>
2023-02-05 17:28:45 +0800

    refactored epics

--- src/store/epics.js
+++ src/store/epics.js
@@ -20,6 +20,16 @@ import {
 
 const url = process.env.REACT_APP_BASE_URL;
 
+const createEndpoint = (state$) => {
+  const {categoryId, offset, search} = state$.value.catalogItems;
+  const query = search ? `&q=${search}` : '';
+  const more = offset !== 0 ? `&offset=${offset}` : '';
+  const category = categoryId ? `categoryId=${categoryId}` : ''
+  const endpoint = `${url}/items?${category}${more}${query}`
+  console.log(endpoint)
+  return endpoint;
+}
+
 export const topSalesEpic = action$ => action$.pipe(
     ofType(topSalesRequest.type),
     switchMap(() => ajax.getJSON(`${url}/top-sales`).pipe(
@@ -32,9 +42,7 @@ export const topSalesEpic = action$ => action$.pipe(
 export const catalogItemsEpic = (action$, state$) => action$.pipe(
     ofType(catalogItemsRequest.type, categoryIdSelect.type),
     switchMap(() => {
-            const {categoryId, offset, search} = state$.value.catalogItems;
-            const query = search ? `&q=${search}` : '';
-            const endpoint = categoryId ? `${url}/items?categoryId=${categoryId}&offset=${offset}${query}` : `${url}/items?offset=${offset}${query}`;
+            const endpoint = createEndpoint(state$);
             return ajax.getJSON(endpoint).pipe(
                 map(res => catalogItemsSuccess(res)),
                 catchError(error => of(catalogItemsFailure(error)))
@@ -47,9 +55,7 @@ export const loadMoreEpic = (action$, state$) =>
   action$.pipe(
     ofType(loadMoreRequest.type),
     switchMap(() => {
-        const {categoryId, offset, search} = state$.value.catalogItems;
-        const query = search ? `&q=${search}` : '';
-        const endpoint = categoryId ? `${url}/items?categoryId=${categoryId}&offset=${offset}${query}` : `${url}/items?offset=${offset}${query}`;
+        const endpoint = createEndpoint(state$);
       return ajax
         .getJSON(endpoint)
         .pipe(
